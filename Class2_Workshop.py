f9a99f77405ec81b20d6a93d3a26c7437033ce2e
BKZ-TheLazyDev <190689953+beamkungza1234@users.noreply.github.com>
2025-07-23 00:40:09 +0700

    Add ValueError check when type text in code

--- Class2_Workshop.py
+++ Class2_Workshop.py
@@ -1,24 +1,28 @@
-dist = input("Distance Price Calulator\nEnter Distance: ")
-dist = int(dist)
+try:
+    dist = input("Distance Price Calulator\nEnter Distance: ")
+    dist = int(dist)
 
-#debug
-print(type(dist))
+    #debug
+    print(type(dist))
 
-print("------------------------------------------------------")
+    print("------------------------------------------------------")
 
-if dist <= 4:
-    print("Your distance is: ", dist, "\nNo price included")
-elif 5 <= dist <= 50:
-    print("Your distance is: ", dist, "\nPrice is 10 Bath")
-elif 51 <= dist <= 100:
+    if dist <= 4:
+        print("Your distance is: ", dist, "\nNo price included")
+    elif 5 <= dist <= 50:
+        print("Your distance is: ", dist, "\nPrice is 10 Bath")
+    elif 51 <= dist <= 100:
         print("Your distance is: ", dist, "\nPrice is 15 Bath")
-elif 101 <= dist <= 300:
-    print("Your distance is: ", dist, "\nPrice is 25 Bath")
-elif 301 <= dist <= 500:
-    print("Your distance is: ", dist, "\nPrice is 35 Bath")
-elif dist > 500:
-    print("Your distance is: ", dist, "\nPrice is 45 Bath")
-else:
-    print("Unknow distance. Please try again")
+    elif 101 <= dist <= 300:
+        print("Your distance is: ", dist, "\nPrice is 25 Bath")
+    elif 301 <= dist <= 500:
+        print("Your distance is: ", dist, "\nPrice is 35 Bath")
+    elif dist > 500:
+        print("Your distance is: ", dist, "\nPrice is 45 Bath")
+    else:
+        print("Unknow distance. Please try again")
+
+except ValueError:
+    print(f"-------------------------------------------\n[!] Your distance value just like a '{dist}'?? You are cooked bro\n-------------------------------------------")
         
      
