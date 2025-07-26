f87d3c09990c74051aaf12104cb8629704b83759
Weerawit <190689953+beamkungza1234@users.noreply.github.com>
2025-07-27 00:52:02 +0700

    Added Rate 'G' Supported in code

--- Class4_Workshop.py
+++ Class4_Workshop.py
@@ -1,47 +1,37 @@
 movieList = [
-    {"id": 1, "name": "Skibidi Toliet", "price": 30, "minAge": 6, "type": "Fantasy"},
-    {"id": 2, "name": "Badass Basic Programing", "price": 40, "minAge": 15, "type": "Documentary"},
+    {"id": 1, "name": "Skibidi Toliet", "price": 30, "minAge": 10, "type": "Fantasy"},
+    {"id": 2, "name": "Badass Basic Programing", "price": 40, "minAge": "G", "type": "Documentary"},
     {"id": 3, "name": "Robood The Movie", "price": 35, "minAge": 13, "type": "Horror"},
     {"id": 4, "name": "Supersigma", "price": 45, "minAge": 18,  "type": "Action"},
     {"id": 5, "name": "IT Startpack IT31", "price": 30, "minAge": 15,  "type": "Scifi"},
     {"id": 6, "name": "Beasts and the frog", "price": 45, "minAge": 16,  "type": "Romantic"},
-    {"id": 7, "name": "Yohane the Parhelion", "price": 50, "minAge": 13,  "type": "Fantasy"},
+    {"id": 7, "name": "Yohane the Parhelion", "price": 50, "minAge": "G",  "type": "Fantasy"},
 ]
-
-
-# def movieChoice():
-#     movie = int(input("Choose Movie to watching\n>> "))
-#     if movie == 1:
-#         return(movieList[0])
-#     if movie == 2:
-#         return(movieList[1])
-#     if movie == 3:
-#         return(movieList[2])
-#     if movie == 4:
-#         return(movieList[3])
-#     if movie == 5:
-#         return (movieList[4])
     
+# ขอยาดจดไว้กันลืม xD
 
 choice = []
 # นำเลขหนังที่เลือก จาก movie(int)มาลบกับเลข index ของ movieList ทีแสดงข้อมูลหนังทั้งหมด เพื่อเลือกหนังที่ถูกต้อง
 def movieChoice():
-    movie = int(input("Choose Movie to watching\n>> "))
+    movie = int(input("Choose Movie to watching (If you dont know what movie we have. Pls visit our movie list first by Press [1] on welcome screen)\n>> "))
     if 1 <= movie <= 7:
         return movieList[movie - 1]
     return None       
 
 def soundtrack():
-    st = int(input("Select your sountrack:\n[1] ภาษาไทย (TH)\n[2] English (EN)\n[3] 日本語 (JP)\n>> "))
+    st = int(input("\nSelect your sountrack:\n[1] ภาษาไทย (TH)\n[2] English (EN)\n[3] 日本語 (JP)\n>> "))
     if st == 1:
         st = "ภาษาไทย (TH)"
         return st
+    
     elif st == 2:
         st = "English (EN)"
         return st
+    
     elif st == 3:
         st = "日本語 (JP)"
         return st
+    
     return None
 
 def inputAge():
@@ -56,15 +46,15 @@ while True:
     print("SIT Theather Welcome! - Please select an options\n[1] Show movie list\n[2] Choose Move")
     sel = int(input(">> "))
     if sel == 1:
-        print("\nNo. | Name                      | Type            | Price")
-        print("------------------------------------------------------------")
+        print("\nNo. | Name                      | Type            | Price    | Rate")
+        print("---------------------------------------------------------------------")
         for mlist in movieList:
             # เว้นช่องไฟเพื่อความสวยงาม
-            print(f"{mlist['id']:>2}  | {mlist['name']:<25} | {mlist['type']:<15} | {mlist['price']}")
+            print(f"{mlist['id']:>2}  | {mlist['name']:<25} | {mlist['type']:<15} | {mlist['price']:<8} | {mlist['minAge']}")
         print("\n")
 
     elif sel == 2:
-        selected = movieChoice()
+        selected = movieChoice()  #รบค่าจาก func และทำตามขั้นตอนต่อ <เก็บข้อมูลหนังที่เลือกมาจากรายการใน Movielist>
         if not selected:
             print("Invalid movie selection.")
             continue
@@ -79,12 +69,12 @@ while True:
             print("invalid soundtrack")
             continue
 
-        if age >= selected['minAge']:
+        if selected['minAge'] == 'G' or age >= selected['minAge']:
             final_price = selected['price']
             if selected['type'] == "Fantasy":
                 final_price += 50
         
-            print(f"[i] You can watch '{selected['name']}'")
+            print(f"-----------------------------------------------------------------------\n[i] You can watch '{selected['name']}'")
             choice.append({
                 "movie": selected['name'],
                 "age": age,
@@ -99,10 +89,11 @@ while True:
         print("\nYour current choices:")
         for c in choice:
             if selected['type'] == "Fantasy":
-                print(f"- {c['movie']} (Price: {c['price']}), Sountrack: {c['st']}\n[i] Included add 50 bath charge for Fantasy movie\n\nEnjoy your movie <3")
+                print(f"- {c['movie']} (Price: {c['price']} Bath), Sountrack: {c['st']}\n[i] Included add 50 bath charge for Fantasy movie (from {selected['price']} bath)\n\nEnjoy your movie <3")
             else:
-                print(f"- {c['movie']} (Price: {c['price']}), Sountrack: {c['st']}\n\nEnjoy your movie <3")
-        break 
+                print(f"- {c['movie']} (Price: {c['price']} Bath), Sountrack: {c['st']}\n\nEnjoy your movie <3")
+        break
+
     else:
         print("Unknown selection Cancelled!")
         break
